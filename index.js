437f637f8bbbbff4759a4854e3f292f2151651a0
Agniya <ivanishyna@gmail.com>
2019-04-11 14:16:31 +0300

    add server file

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/moviesDB', { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
+//init routes
+app.use('/api', require('./routes/api'));
+
+//error handling middleware
+app.use(function(err, req, res, next){
+  res.status(422).send({error: err.message});
+});
+
+//listen for request
+app.listen(process.env.port || 4000, function(){
+  console.log("Hello, now listening for request ")
+})

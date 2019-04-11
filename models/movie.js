3e2745fda5b1f1b16f31f4609e38e315f8c24784
Agniya <ivanishyna@gmail.com>
2019-04-11 14:14:32 +0300

    add db model

---
+++ models/movie.js
@@ -0,0 +1,30 @@
+/***
+  provide Schema
+*/
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+  title:  {
+    type: String,
+    required:[true, 'Filed is required']
+  },
+  year: {
+    type: Number,
+    required:[true, 'Filed is required']
+  },
+  format:  {
+    type: String,
+    required:[true, 'Filed is required']
+  },
+  actors: [
+    {
+      name: String,
+      surname: String
+    }
+  ]
+})
+
+const Movie = mongoose.model('movie', MovieSchema);
+module.exports = Movie;
